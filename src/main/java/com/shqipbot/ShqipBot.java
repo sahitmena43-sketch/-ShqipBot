05dc9cad9155351a5ef4ab50d20d663f2f4834a4
Microsoft Surface <Microsoft Surface@SAHIT>
2026-07-02 20:13:07 +0200

    ShqipBot - Boti ekonomik shqiptar me baste, WebSocket dhe 20 punë alla shqiptare

--- src/main/java/com/shqipbot/ShqipBot.java
+++ src/main/java/com/shqipbot/ShqipBot.java
@@ -1,5 +1,6 @@
 package com.shqipbot;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -7,7 +8,9 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import java.util.Random;
+import java.awt.Color;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class ShqipBot extends ListenerAdapter {
     
@@ -18,6 +21,84 @@ public class ShqipBot extends ListenerAdapter {
     private long lastCommandTime = 0;
     
     private final String ADMIN_ID = "123456789012345678"; // Zëvendëso me ID-në tënde!
+    
+    // ==================== 20 PUNË ALLA SHQIPTARE ====================
+    private final String[][] punet = {
+        {"Shitës Euro në Rrugë", "Shite Euro false por turistët i deshën", "150"},
+        {"Kontrabandist TVSH", "Fshehe TVSH-në në tavan por ra dhe u zbulove", "200"},
+        {"Mësues i Gjuhës Shqipe", "Mësove 'qe' dhe 'ka' por nxënësit thanë 'ka qe'", "100"},
+        {"Menaxher Nëntoke", "Menaxhove biznesin nga bodrumi por policia erdhi", "180"},
+        {"Falsifikues Diplomash", "Bëre diplomë për kafshën por e mori seriozisht", "250"},
+        {"Përkthyes në Gjyq", "Përktheve nga shqip në shqip por gjyqtari u hutua", "120"},
+        {"Shitës Kravatash", "Shite kravata me ngjyra të çmendura por të gjithë i blenë", "90"},
+        {"Konsulent Politik", "Këshillove politikanin por ai bëri të kundërtën", "300"},
+        {"Gazetar Sporti", "Shkruajti lajm të rremë por u besuan", "130"},
+        {"Agjent Imobiliash", "Shite banesë në Tiranë por ishte në Mars", "250"},
+        {"Rregullues Shorti", "Rregullove shortin e Kupës por u zbulove", "400"},
+        {"Kameraman Dasmash", "Xhirove dasmën por ngatërrove nusen me kunatën", "110"},
+        {"Shitës Teli", "Shite tel të ndryshkur si të ri", "70"},
+        {"Noter i Rrugës", "Noterove dokumente pa vizë por i deshën", "160"},
+        {"Eksportues Vaji", "Eksportove vaj ulliri por ishte vaj makine", "220"},
+        {"Trajner Futbolli", "Trajnovat skuadrën por ata luanin basketboll", "190"},
+        {"Shitës Uji", "Shite ujë të paster por ishte nga lumi", "80"},
+        {"Menaxher Kulture", "Organizove koncert por këngëtari ishte i dehur", "140"},
+        {"Krijues Sloganesh", "Krijove slogan por e përdori kundërshtari", "170"},
+        {"Punonjës i BSH", "Punoje në BSH por i ngatërrove statistikat", "210"}
+    };
+    
+    // ==================== SLUT - 10 SUKSESE + 10 DËSHTIME ====================
+    private final String[][] slutSukses = {
+        {"Tregove vallëzim te Pink Floyd", "200"},
+        {"Këndove në dasmë me fustan të shkurtër", "350"},
+        {"Bëre live në TikTok me veshje provokuese", "500"},
+        {"Bëre foto për OnlyFans në Kala", "450"},
+        {"Kërcen në 'Puls' në mes të natës", "400"},
+        {"Ofrove shërbim për shefin e plazhit", "300"},
+        {"Shite trupin në 'New York' për 24 orë", "250"},
+        {"Bëre striptiz në bachelorette party", "380"},
+        {"Fotove veten me Berishën", "420"},
+        {"Shfaqve trupin në makinë te Liqeni", "150"}
+    };
+    
+    private final String[][] slutDeshtim = {
+        {"Ofrove masazh por klienti ishte inspektor", "-100"},
+        {"Të kapën me klient në makinë te Liqeni", "-250"},
+        {"Shefës i ofrove 'shërbim special' për të mbajtur punën", "-300"},
+        {"Ofrove taksi me 'shërbim shtesë' por ishte polic", "-150"},
+        {"Bëre foto për OnlyFans por ishte policia", "-200"},
+        {"Tregove trupin në plazh dhe të arrestuan", "-180"},
+        {"Këndove në dasmë por ishte e fejuara", "-220"},
+        {"Ofrove shërbim në hotel por ishte nusja", "-280"},
+        {"Bëre striptiz në familje", "-350"},
+        {"Shite trupin por ishte vetëm një shaka", "-130"}
+    };
+    
+    // ==================== CRIME - 10 SUKSESE + 10 DËSHTIME ====================
+    private final String[][] crimeSukses = {
+        {"Shite CD të paluajtshme te 'Rruga e Kavajës'", "500"},
+        {"Kontrabandove cigare nga Mali i Zi", "1000"},
+        {"Shite narkotikë te shkolla 'Fan Noli'", "800"},
+        {"Vodhe bakër nga Kantieri", "600"},
+        {"Falsifikove Euro në shtëpi", "1200"},
+        {"Shite armë për bandën e Durrësit", "900"},
+        {"U fute ilegalisht në Greqi", "400"},
+        {"Shite patentë të rreme", "750"},
+        {"Vodhe makinë në Tiranë", "650"},
+        {"Grabitëse dyqani në Vlorë", "550"}
+    };
+    
+    private final String[][] crimeDeshtim = {
+        {"U përpoqe të korruptosh policin", "-300"},
+        {"Të kapën me armë pa leje te 'Farka'", "-500"},
+        {"Grabitëse banke por ra alarmi", "-700"},
+        {"Shite narkotikë por ishte polic në civil", "-400"},
+        {"Kontrabandove duhan dhe të kapën në kufi", "-600"},
+        {"U përpoqe të vjedhësh bankë por u mbyll", "-350"},
+        {"Shite armë por ishin kuti për fëmijë", "-250"},
+        {"Falsifikove dokumenta dhe të zbuluan", "-450"},
+        {"U fute ilegalisht dhe të kthyen mbrapsht", "-200"},
+        {"Vodhe makinë por ishte e policisë", "-550"}
+    };
     
     public ShqipBot(String token) throws Exception {
         this.db = new Database();
@@ -58,48 +139,89 @@ public class ShqipBot extends ListenerAdapter {
         
         db.krijoPerdorues(userId, username);
         
+        // ==================== 'work ====================
         if (command.equals("'work")) {
-            String[][] punet = {
-                {"pastrues në Tiranë", "Fshive rrugët te Blloku", "50"},
-                {"ndërtimtar te Aneks", "Vure tulla por shefi mashtroi", "120"},
-                {"shitës në pazar", "Shite rroba kineze", "80"},
-                {"kamarier në Durrës", "Servire turistët", "70"},
-                {"taksi i fshehtë", "Çove njerëz pa targë", "90"}
-            };
             int index = random.nextInt(punet.length);
             int fitimi = Integer.parseInt(punet[index][2]);
             db.shtoPara(userId, fitimi);
-            event.getChannel().sendMessage("🇦🇱 **" + username + "** punoi si " + 
-                    punet[index][0] + ".\n" + punet[index][1] + "\n💰 +" + fitimi + " lekë").queue();
+            
+            EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Color.GREEN)
+                .setTitle("🇦🇱 PUNË ALLA SHQIPTARE 🇦🇱")
+                .setDescription("**" + event.getAuthor().getName() + "** punoi si **" + punet[index][0] + "**")
+                .addField("📝 Përshkrimi", punet[index][1], false)
+                .addField("💰 Fitimi", "+" + fitimi + " lekë", true)
+                .setFooter("ShqipBot © 2026", null)
+                .setTimestamp(new Date().toInstant());
+            
+            event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
+        
+        // ==================== 'slut ====================
         else if (command.equals("'slut")) {
-            String[][] skenaret = {
-                {"Tregove vallëzim te Pink Floyd", "200"},
-                {"Ofrove masazh por ishte inspektor", "-100"},
-                {"Këndove në dasmë me fustan të shkurtër", "350"},
-                {"Të kapën me klient te Liqeni", "-250"}
-            };
-            int index = random.nextInt(skenaret.length);
-            int shuma = Integer.parseInt(skenaret[index][1]);
+            boolean sukses = random.nextBoolean();
+            String[] result;
+            String status;
+            Color color;
+            
+            if (sukses) {
+                result = slutSukses[random.nextInt(slutSukses.length)];
+                status = "✅ SUKSES";
+                color = Color.GREEN;
+            } else {
+                result = slutDeshtim[random.nextInt(slutDeshtim.length)];
+                status = "❌ DËSHTIM";
+                color = Color.RED;
+            }
+            
+            int shuma = Integer.parseInt(result[1]);
             db.shtoPara(userId, shuma);
-            String shenja = (shuma > 0) ? "+" : "";
-            event.getChannel().sendMessage("🍑 **" + username + "**: " + 
-                    skenaret[index][0] + "\n💰 " + shenja + shuma + " lekë").queue();
+            
+            EmbedBuilder embed = new EmbedBuilder()
+                .setColor(color)
+                .setTitle("🍑 SHËRBIME TË DYSHIMTA 🍑")
+                .setDescription("**" + event.getAuthor().getName() + "** " + result[0])
+                .addField("📊 Statusi", status, true)
+                .addField("💰 Rezultati", (shuma > 0 ? "+" : "") + shuma + " lekë", true)
+                .setFooter("ShqipBot © 2026", null)
+                .setTimestamp(new Date().toInstant());
+            
+            event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
+        
+        // ==================== 'crime ====================
         else if (command.equals("'crime")) {
-            String[][] krime = {
-                {"Shite CD te Rruga e Kavajës", "500"},
-                {"U përpoqe të korruptosh policin", "-300"},
-                {"Kontrabandove cigare nga Mali i Zi", "1000"},
-                {"Të kapën me armë pa leje te Farka", "-500"}
-            };
-            int index = random.nextInt(krime.length);
-            int shuma = Integer.parseInt(krime[index][1]);
+            boolean sukses = random.nextBoolean();
+            String[] result;
+            String status;
+            Color color;
+            
+            if (sukses) {
+                result = crimeSukses[random.nextInt(crimeSukses.length)];
+                status = "✅ SUKSES";
+                color = Color.GREEN;
+            } else {
+                result = crimeDeshtim[random.nextInt(crimeDeshtim.length)];
+                status = "❌ DËSHTIM";
+                color = Color.RED;
+            }
+            
+            int shuma = Integer.parseInt(result[1]);
             db.shtoPara(userId, shuma);
-            String shenja = (shuma > 0) ? "+" : "";
-            event.getChannel().sendMessage("😈 **" + username + "**: " + 
-                    krime[index][0] + "\n💰 " + shenja + shuma + " lekë").queue();
+            
+            EmbedBuilder embed = new EmbedBuilder()
+                .setColor(color)
+                .setTitle("😈 PUNË KRIMINALE 😈")
+                .setDescription("**" + event.getAuthor().getName() + "** " + result[0])
+                .addField("📊 Statusi", status, true)
+                .addField("💰 Rezultati", (shuma > 0 ? "+" : "") + shuma + " lekë", true)
+                .setFooter("ShqipBot © 2026", null)
+                .setTimestamp(new Date().toInstant());
+            
+            event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
+        
+        // ==================== 'rob @user ====================
         else if (command.matches("^'rob @\\w+$")) {
             String targetName = command.split("@")[1];
             User target = null;
@@ -114,81 +236,115 @@ public class ShqipBot extends ListenerAdapter {
                 event.getChannel().sendMessage("❌ Nuk mund të gjej përdoruesin.").queue();
                 return;
             }
+            
             if (target == null || target.getId().equals(userId)) {
                 event.getChannel().sendMessage("❌ Nuk mund të grabisësh veten.").queue();
                 return;
             }
+            
             int shuma = random.nextInt(300) + 50;
             boolean suksesi = random.nextDouble() > 0.4;
             int balanceViktima = db.merrBalance(target.getId());
+            
+            EmbedBuilder embed = new EmbedBuilder()
+                .setColor(suksesi ? Color.GREEN : Color.RED)
+                .setTitle("🔫 GRABISTJE 🔫");
+            
             if (suksesi && balanceViktima >= shuma) {
                 db.zbritPara(target.getId(), shuma);
                 db.shtoPara(userId, shuma);
-                event.getChannel().sendMessage("🔫 **" + username + "** grabisi @" + 
-                        targetName + " dhe mori " + shuma + " lekë! 💰").queue();
+                embed.setDescription("**" + event.getAuthor().getName() + "** grabisi @" + targetName)
+                    .addField("💰 Shuma", "+" + shuma + " lekë", true)
+                    .addField("🎯 Viktima", "@" + targetName, true)
+                    .addField("📊 Statusi", "✅ SUKSES", true);
             } else {
                 db.zbritPara(userId, 200);
-                event.getChannel().sendMessage("🚔 Grabistja dështoi! **" + username + 
-                        "** u kap dhe pagoi 200 lekë gjobë.").queue();
+                embed.setDescription("**" + event.getAuthor().getName() + "** u kap duke grabisur @" + targetName)
+                    .addField("💰 Humbja", "-200 lekë", true)
+                    .addField("🚔 Statusi", "❌ DËSHTIM", true);
             }
+            
+            embed.setFooter("ShqipBot © 2026", null)
+                .setTimestamp(new Date().toInstant());
+            
+            event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
+        
+        // ==================== 'bal ====================
         else if (command.equals("'bal")) {
             int balance = db.merrBalance(userId);
-            event.getChannel().sendMessage("💰 **" + username + "** ka " + balance + 
-                    " lekë në xhep.\n📡 Ping: " + jda.getGatewayPing() + " ms").queue();
+            
+            EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Color.CYAN)
+                .setTitle("💰 GJENDJA FINANCIARE 💰")
+                .setDescription("**" + event.getAuthor().getName() + "** ka **" + balance + "** lekë në xhep!")
+                .addField("📡 Ping", jda.getGatewayPing() + " ms", true)
+                .setFooter("ShqipBot © 2026", null)
+                .setTimestamp(new Date().toInstant());
+            
+            event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
+        
+        // ==================== 'dep ====================
         else if (command.equals("'dep")) {
             long depozitaFundit = db.merrDepozitaFundit(userId);
             long tani = System.currentTimeMillis();
             long diferenca = tani - depozitaFundit;
+            
+            EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Color.YELLOW)
+                .setTitle("🏦 DEPOZITO BONUS DITOR 🏦");
+            
             if (diferenca < 86400000) {
                 long oreMbetur = (86400000 - diferenca) / 3600000;
-                event.getChannel().sendMessage("⏳ Ke marrë bonusin sot. Provo pas " + oreMbetur + " orësh!").queue();
+                embed.setDescription("⏳ Ke marrë bonusin sot!")
+                    .addField("⏰ Provo pas", oreMbetur + " orësh", true);
             } else {
                 db.shtoPara(userId, 100);
                 db.updateDepozita(userId, tani);
-                event.getChannel().sendMessage("🏦 Depozitove 100 lekë bonus ditor! 💰").queue();
+                embed.setDescription("🎉 **" + event.getAuthor().getName() + "** depozitoi **100 lekë** bonus ditor!")
+                    .setColor(Color.GREEN)
+                    .addField("💰 Bonus", "+100 lekë", true);
             }
+            
+            embed.setFooter("ShqipBot © 2026", null)
+                .setTimestamp(new Date().toInstant());
+            
+            event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
+        
+        // ==================== 'with ====================
         else if (command.equals("'with")) {
             db.zbritPara(userId, 100);
             int balance = db.merrBalance(userId);
-            event.getChannel().sendMessage("💸 Tërhoqe 50 lekë por taksohesh 50 lekë gjobë.\n" +
-                    "Gjendja e re: **" + balance + "** lekë").queue();
+            
+            EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Color.ORANGE)
+                .setTitle("💸 TËRHIQJE PARASH 💸")
+                .setDescription("**" + event.getAuthor().getName() + "** tërhoqi 50 lekë por taksohesh 50 lekë!")
+                .addField("💰 Gjendja e re", balance + " lekë", true)
+                .addField("💸 Takse", "-100 lekë", true)
+                .setFooter("ShqipBot © 2026", null)
+                .setTimestamp(new Date().toInstant());
+            
+            event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
-        else if (command.startsWith("/add_money") && userId.equals(ADMIN_ID)) {
-            String[] parts = command.split(" ");
-            if (parts.length == 3 && parts[1].startsWith("@")) {
-                String targetUser = parts[1].substring(1);
-                try {
-                    int amount = Integer.parseInt(parts[2]);
-                    event.getChannel().sendMessage("✅ I shtove @" + targetUser + " " + amount + " lekë.").queue();
-                } catch (NumberFormatException e) {
-                    event.getChannel().sendMessage("❌ Shuma duhet të jetë numër!").queue();
-                }
-            }
-        }
+        
+        // ==================== 'help ====================
         else if (command.equals("'help")) {
-            event.getChannel().sendMessage(
-                "🇦🇱 **SHQIPBOT - Boti Ekonomik Shqiptar**\n\n" +
-                "`'work` - Puno në Shqipëri (50-120 lekë)\n" +
-                "`'slut` - Shërbime të dyshimta (-300 deri +500)\n" +
-                "`'crime` - Punë kriminale (-700 deri +1200)\n" +
-                "`'rob @user` - Grabiste dikë (50-350 lekë)\n" +
-                "`'bal` - Shiko sa lekë ke\n" +
-                "`'dep` - Bonus ditor 100 lekë\n" +
-                "`'with` - Tërhiq para (humb 100 lekë)\n\n" +
-                "💓 Ping: " + jda.getGatewayPing() + " ms\n" +
-                "📢 Bërë me 🇦🇱 për Shqipërinë"
-            ).queue();
-        }
-        else if (command.equals("'start")) {
-            event.getChannel().sendMessage(
-                "🇦🇱 **MIRË SE VJEN NË SHQIPBOT!**\n\n" +
-                "Boti ekonomik 100% në gjuhën shqipe 🇦🇱\n\n" +
-                "Provo: `'work`, `'bal`, `'help`\n" +
-                "💰 Fillo me 100 lekë në xhep!"
-            ).queue();
+            EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Color.BLUE)
+                .setTitle("🇦🇱 SHQIPBOT - Lista e Komandave 🇦🇱")
+                .setDescription("Boti ekonomik 100% në gjuhën shqipe!")
+                .addField("💼 **PUNË**", "`'work` - Puno në Shqipëri (20 punë alla shqiptare)", false)
+                .addField("🍑 **SHËRBIME**", "`'slut` - Shërbime të dyshimta", false)
+                .addField("😈 **KRIM**", "`'crime` - Punë kriminale", false)
+                .addField("🔫 **GRABISTJE**", "`'rob @user` - Grabiste dikë", false)
+                .addField("💰 **FINANCA**", "`'bal` - Gjendja\n`'dep` - Bonus ditor\n`'with` - Tërhiq", false)
+                .setFooter("ShqipBot © 2026", null)
+                .setTimestamp(new Date().toInstant());
+            
+            event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
     }
 }
